f6ac4a7b985b9b3a22296a3f099078b84caa0838
joverdejo <joverdejo@gmail.com>
2021-03-28 23:53:20 -0400

    Initial Commit

---
+++ MeatMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeatMonitor
+//
+//  Created by Joshua Verdejo on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
